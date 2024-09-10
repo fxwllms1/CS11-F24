cb3400af65148047d5250e33d8ac496550e4ed1e
Steph <96219204+steph1111@users.noreply.github.com>
2024-09-10 12:55:18 -0700

    Update phone_num.cpp

---
+++ week_3/tuesday/ex01/phone_num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// variable to store the phone number
+
+  cout << "Enter a phone number: ";
+	// take input for the phone number
+
+  // output the number with the specified formatting
+}
