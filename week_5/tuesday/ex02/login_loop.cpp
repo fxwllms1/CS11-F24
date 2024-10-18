6b7de159c7c4070deb8221da4397d76163577c98
fxwllms1 <fxwllms1@gmail.com>
2024-10-17 21:49:45 -0700

    complete

--- week_5/tuesday/ex02/login_loop.cpp
+++ week_5/tuesday/ex02/login_loop.cpp
@@ -1,1 +1,34 @@
-// Your code from the last exercise here
+// program asks user for email and password and then determins if its correct, looping re inout for incorrect
+#include <iostream>
+using namespace std;
+// program accepts inout of email and password from user and determins if it is correct
+int main() {
+    // Constants (correct values)
+    const string EMAIL = "stlheure@cabrillo.edu";
+    const string PASSWORD = "password123";
+
+    string email, password;
+
+    cout << "Email: ";
+    cin >> email;
+
+    cout << "Password: ";
+    cin >> password;
+
+    if (email == EMAIL && password == PASSWORD) {
+        cout << "Welcome to the site" << endl;
+    }
+    while (email != EMAIL || password != PASSWORD) {
+        cout << "Incorrect email or password, please re enter" << endl;
+        cout << "Email: ";
+        cin >> email;
+
+        cout << "Password: ";
+        cin >> password;
+    }
+    // if (successful login)
+    // welcome user
+    // if (unsuccessful login)
+    // email or password is incorrect
+
+}
