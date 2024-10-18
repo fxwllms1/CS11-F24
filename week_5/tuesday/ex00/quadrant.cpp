e8717cf0d0bbfa9ee7ab4158ddbe6082bb5ba50c
fxwllms1 <fxwllms1@gmail.com>
2024-10-17 20:45:06 -0700

    complete

--- week_5/tuesday/ex00/quadrant.cpp
+++ week_5/tuesday/ex00/quadrant.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+// Program takes inpput of x and y and determins postions of points on a graph
 int main() {
 	double x, y;
 
@@ -8,7 +8,29 @@ int main() {
     cin >> x;
     cout << "y: ";
     cin >> y;
-
-    cout << "The point (" << x << ", " << y << ") lies ";
+    string position;
+// quadrant I
+    if (y > 0 && x > 0) {
+        position = "within QI";
+    }
+    else if (y > 0 && x < 0) {
+        position = "within QII";
+    }
+    else if (y < 0 && x < 0) {
+        position = "within QIII";
+    }
+    else if (y > 0 && x > 0) {
+        position = "within QV";
+    }
+    else if (y == 0 && x == 0) {
+        position = "on the origin";
+    }
+    else if (y == 0) {
+        position = "on the x axis";
+    }
+    else if (x == 0) {
+        position = "on the y axis";
+    }
+    cout << "The point (" << x << ", " << y << ") lies " << position << endl;;
     
 }
