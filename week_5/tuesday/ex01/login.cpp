f1f8b37b3af4f7b5f5fcf2cdfe5b942f98b79e56
fxwllms1 <fxwllms1@gmail.com>
2024-10-17 20:58:33 -0700

    complete

--- week_5/tuesday/ex01/login.cpp
+++ week_5/tuesday/ex01/login.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-
+// program accepts inout of email and password from user and determins if it is correct
 int main() {
     // Constants (correct values)
-    const string EMAIL = "FIXME";
-    const string PASSWORD = "FIXME";
+    const string EMAIL = "stlheure@cabrillo.edu";
+    const string PASSWORD = "password123";
 
     string email, password;
 
@@ -14,6 +14,12 @@ int main() {
     cout << "Password: ";
     cin >> password;
 
+    if (email == EMAIL && password == PASSWORD) {
+        cout << "Welcome to the site" << endl;
+    }
+    else {
+        cout << "email or password is incorrect" << endl;
+    }
     // if (successful login)
     // welcome user
     // if (unsuccessful login)
