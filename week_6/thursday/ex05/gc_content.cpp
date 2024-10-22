3de4cec1da4f633d4c005ddaceb280893d422b62
fxwllms1 <fxwllms1@gmail.com>
2024-10-21 22:08:16 -0700

    finished

--- week_6/thursday/ex05/gc_content.cpp
+++ week_6/thursday/ex05/gc_content.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 int main () {
 	string sequence;
-	cin >> sequence;
-
+	int total =  0;
+	int gc_total = 0;
+	int gc_percent = 0;
     // Your code here
-    
+	while (cin >> sequence)
+		for (int i = 0; i < sequence.length(); i++) {
+			char c;
+			c = sequence[i];
+			if (c == 'A' || c == 'C' || c == 'G' || c == 'T') {
+				total++;
+			}
+			if (c == 'C' || c == 'G') {
+				gc_total++;
+			}
+		}
+
+	gc_percent = (1.0 * gc_total / sequence.length()) * 100; 
+	cout << "percent of 'G' and 'C' is: " << gc_percent << "%" << endl;
+
+	while (cin.eof()) {
+		break;
+	}
+	
 	return 0;
 }
