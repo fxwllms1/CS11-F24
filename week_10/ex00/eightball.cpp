c959d82dc4667983796bc4a49ff80a69f4ec071b
steph1111 <stephanielh1111@gmail.com>
2024-10-28 14:19:04 -0700

    Only one session for week 9

---
+++ week_10/ex00/eightball.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    const int SIZE = 20;
+    string responses[SIZE] = {
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes, definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    };
+    string input;
+
+    cout << "Ask a question: ";
+    getline(cin, input);
+
+    // Output a random element from the array
+
+    return 0;
+}
