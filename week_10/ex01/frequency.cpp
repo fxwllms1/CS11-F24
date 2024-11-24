7e06a713b820227ec13d8297101681782070f6a2
fxwllms1 <fxwllms1@gmail.com>
2024-11-23 18:37:00 -0800

    passed

--- week_10/ex01/frequency.cpp
+++ week_10/ex01/frequency.cpp
@@ -10,14 +10,21 @@ using namespace std;
  * @param target Element to count frequency.
  * @return Number of times `target` appears in `a`.
  */
-int frequency(string a[], int size, string target);
+int frequency(string a[], int size, string target) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (a[i] == target) {
+            count++;
+        }
+    }
+    return count;
+}
 
 int main() {
     string foods[7]     = {"Pizza", "Burger", "Pasta", "Sushi", "Pizza", "Sushi", "Sushi"};
     string colors[8]    = {"Red", "Blue", "Green", "Yellow", "Blue", "Green", "Yellow", "Blue"};
     string cars[10]     = {"Toyota", "Honda", "Ford", "BMW", "Honda", "Toyota", "Audi", "Chevrolet", "BMW", "Ford"};
 
-    // Assertion tests (uncomment when ready to test)
     assert(frequency(foods, 7, "Sushi") == 3);
     assert(frequency(colors, 8, "Green") == 2);
     assert(frequency(cars, 10, "Audi") == 1);
