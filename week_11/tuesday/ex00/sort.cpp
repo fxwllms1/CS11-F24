f83bbb29188f4e9786f950e3362d07802ab05bd2
fxwllms1 <fxwllms1@gmail.com>
2024-11-23 20:20:59 -0800

    done and sorted

--- week_11/tuesday/ex00/sort.cpp
+++ week_11/tuesday/ex00/sort.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-// Include libs
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
     // Create a vector
+    vector<double> input;
 
     // Take input till EOF and add to a vector 
+    while(!cin.eof()) {
+        double vinput;
+        cin >> vinput;
+        input.push_back(vinput);
+    }
+    vector<double> original = input; // saving original vector
+    int size = input.size();
 
     // Sort the vector (later)
+    sort(input.begin(), input.end());
+        
 
     // Output each element
-
+    for(int i = 0; i < size; i++) {
+        cout << input[i] << endl;
+    }
+    cout << endl;
     return 0;
 }
